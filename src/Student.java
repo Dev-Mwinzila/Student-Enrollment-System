d263d1cf8de619cec10fdfa4b43df06677fbbcc3
Lincohn Nwinzila <Mwinzilalincohn@outlook.com>
2023-06-12 14:18:29 +0300

    A student management system

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+// constructor to prompt user to add students name and year
+    //Generate id
+    //Enrol in a course
+    //view balance
+    //pay tuition fees
+    private String firstName;
+    private  String lastName;
+    private  String  studentID;
+    private  String gradeYear;
+    private String courses;
+    private String 
+
+}
---
+++ src/StudentDatabaseApp.java
@@ -0,0 +1,7 @@
+public class StudentdatabaseApp {
+ public static void main(String[]args){
+     //Ask how many students  will be added
+     // create n number of new students
+     //show status
+ }
+}
