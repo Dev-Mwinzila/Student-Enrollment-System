69b519bbccea611455b404e901cea09ebcfbc8c4
Dev_Mwinzila <Mwinzilalincohn@outlook.com>
2023-06-13 08:52:13 +0300

    Enroll and payment features added

--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,63 @@
-public class Student {
-// constructor to prompt user to add students name and year
-    //Generate id
-    //Enrol in a course
-    //view balance
-    //pay tuition fees
-    private String firstName;
-    private  String lastName;
-    private  String  studentID;
-    private  String gradeYear;
-    private String courses;
-    private String 
+import java.util.Scanner;
 
+public class Student {
+    private final String firstName;
+    private final String lastName;
+    private String studentID;
+    private final int gradeYear;
+    private String courses;
+    public static int id = 1000;
+    private int tuitionBalance;
+
+    private static final int costOfCourse = 600;
+
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student's First Name");
+        this.firstName = in.nextLine();
+        System.out.println("Enter student's Last Name");
+        this.lastName = in.nextLine();
+        System.out.println("1 - Freshmen\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student's Class level:");
+        this.gradeYear = in.nextInt();
+        setStudentID();
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+    }
+
+    private void setStudentID() {
+        id++;
+        this.studentID = gradeYear + String.valueOf(id);
+    }
+
+    public void enroll() {
+        String course;
+        do {
+            System.out.println("Enter course to enroll (Q to quit):");
+            Scanner in = new Scanner(System.in);
+            course = in.nextLine();
+            if (!course.equalsIgnoreCase("Q")) {
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            } else {
+                break;
+            }
+        } while (1!=1);
+
+        System.out.println("Enrolled in: " + courses);
+        System.out.println("Tuition Balance: " + tuitionBalance);
+
+        // Process payment
+        processPayment();
+    }
+
+    void processPayment() {
+        // Here, you can integrate your payment API code to process the payment
+        // You can make API requests, handle responses, and update the tuition balance accordingly
+        // This is just a placeholder method for demonstration purposes
+        System.out.println("Processing payment...");
+
+        // Simulate a successful payment
+        tuitionBalance = 0;
+        System.out.println("Payment processed successfully.");
+        System.out.println("Updated Tuition Balance: " + tuitionBalance);
+    }
 }
--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -1,7 +1,12 @@
-public class StudentdatabaseApp {
+public class StudentDatabaseApp {
  public static void main(String[]args){
-     //Ask how many students  will be added
-     // create n number of new students
-     //show status
+/*
+     Ask how many students  will be added
+      create n number of new students
+     show status
+*/
+  Student stu1 = new Student();
+  stu1.enroll();
+  stu1.processPayment();
  }
 }
